1ff80a9c89e0ca50bf41881768c9e503cd5bdd85
leetailoong <leetailoong@outlook.com>
2018-10-07 06:41:03 +0800

    adjust alias method chain

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -13,6 +13,9 @@ Spree::Order.class_eval do
   end
   alias_method_chain :finalize!, :gift_card
 
+  alias_method :finalize_without_gift_card, :finalize!
+  alias_method :finalize!, :finalize_with_gift_card!
+
   # Tells us if there is the specified gift code already associated with the order
   # regardless of whether or not its currently eligible.
   def gift_credit_exists?(gift_card)
