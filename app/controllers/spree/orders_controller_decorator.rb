ca3d60ad328516ecd0a9ef3454384e930d004225
leetailoong <leetailoong@outlook.com>
2018-11-09 01:55:34 +0800

    adjust

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,7 +1,7 @@
-Spree::OrdersController.class_eval do
+# Spree::OrdersController.class_eval do
 
-  Spree::PermittedAttributes.checkout_attributes << :gift_code
+#   Spree::PermittedAttributes.checkout_attributes << :gift_code
 
-  after_action :apply_gift_code, only: :update
+#   after_action :apply_gift_code, only: :update
 
-end
+# end
